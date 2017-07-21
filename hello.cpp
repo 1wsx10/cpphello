ab2934aed0da936e016306622c53da541b7e9798
1wsx10 <1wsx10@gmail.com>
2017-07-21 13:30:34 +1000

    first commmit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using <std>
+
+int main(int argc, char **argv) {
+	char asdf[20];
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "i like to live dangerously\n";
+
+	std::cin >> asdf;
+	std::cout << asdf << "\n";
+
+	return 0;
+}
